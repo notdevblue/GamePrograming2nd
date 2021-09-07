634176fa51cbfa19c8285d1ebfc6cd17f4f208d0
Han <wooyeophan@gmail.com>
2021-09-07 13:56:30 +0900

    2번 과제 푸쉬

--- ex_02.php
+++ ex_02.php
@@ -1,4 +1,45 @@
 <?php
 
+	$size = 10;
+    $starArr = array();
+
+	for($i = 0; $i < $size / 2; ++$i)
+	{
+		RepeatForFull($i, "&nbsp");
+		RepeatForFull($size / 2 - $i, "*");
+        RepeatForFull($size / 2 - $i, "&nbsp");
+		RepeatForFull($i + 1, "*");
+
+		$starArr[] = "<br>";
+	}
+
+	for($i = 0; $i < $size / 2; ++$i)
+	{
+		RepeatForFull($size / 2 - $i, "*");
+		RepeatForFull($i, "&nbsp");
+		RepeatForFull($i + 1, "*");
+
+		$starArr[] = "<br>";
+	}
+
+    function RepeatForFull($i, $str)
+    {
+        global $starArr;
+
+		for($j = 0; $j < $i; ++$j)
+		{
+			$starArr[] = $str;
+		}
+    }
+
+    // for($i = 0; $i < count($starArr); ++$i)
+    // {
+    //     echo $starArr[$i];
+    // }
+
+    foreach($starArr as $key => $value)
+    {
+        echo $value;
+    }
 
 ?>
